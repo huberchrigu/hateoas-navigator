56c9c640bb88c3b72d7de5ce8549f8f5b46d8888
Christoph Huber <huber.chrigu@gmail.com>
2018-01-05 11:51:52 +0100

    Updated dependencies and removed unused config file

--- src/app/hal-navigator/descriptor/json-schema/json-schema-descriptor.spec.ts
+++ src/app/hal-navigator/descriptor/json-schema/json-schema-descriptor.spec.ts
@@ -1,6 +1,7 @@
 import {JsonSchemaDescriptor} from '@hal-navigator/descriptor/json-schema/json-schema-descriptor';
 import {JsonSchema} from 'app/hal-navigator/schema/json-schema';
 import {SchemaReferenceFactory} from 'app/hal-navigator/schema/schema-reference-factory';
+import {FormFieldType} from '@hal-navigator/form/form-field-type';
 
 describe('JsonSchemaDescriptor', () => {
   it('should get referenced child', () => {
@@ -27,5 +28,20 @@ describe('JsonSchemaDescriptor', () => {
 
     expect(testee.getChild('child').getTitle()).toEqual('Child');
     expect(referenceFactoryMock.getReferencedSchema).toHaveBeenCalled();
+  });
+
+  it('should provide an array form field of uris', () => {
+    const array: JsonSchema = {
+      type: 'array',
+      items: {
+        type: 'string',
+        format: 'uri'
+      }
+    };
+    const testee = new JsonSchemaDescriptor('testee', array, null, {} as SchemaReferenceFactory);
+    const result = testee.toFormField();
+
+    expect(result.type).toEqual(FormFieldType.ARRAY);
+    expect(result.options.getArraySpec().type).toEqual(FormFieldType.LINK);
   });
 });
