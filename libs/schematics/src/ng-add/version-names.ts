8edbb1e09c5126863611541cc816d81d7842a460
huber.chrigu <huber.chrigu@gmail.com>
2019-08-02 12:50:33 +0200

    Updated dependencies and set versions to 0.1.0-RC2

--- libs/schematics/src/ng-add/version-names.ts
+++ libs/schematics/src/ng-add/version-names.ts
@@ -1,3 +1,3 @@
 /** Name of the version that is shipped together with the schematics. */
-export const resourceComponentsVersion = '0.0.2-RC3';
-export const hateoasNavigatorVersion = '0.0.2-RC';
+export const resourceComponentsVersion = '0.1.0-RC2';
+export const hateoasNavigatorVersion = '0.1.0-RC2';
