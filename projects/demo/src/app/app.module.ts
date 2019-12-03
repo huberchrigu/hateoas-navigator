557dce98d524f815dd115e5f9df00203c01d1e5e
huber.chrigu <huber.chrigu@gmail.com>
2019-12-03 10:09:37 +0100

    Test suggestion queries

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -5,10 +5,9 @@ import {AppComponent} from './app.component';
 import {StaticComponentsModule} from './static-components/static-components.module';
 import {AppRoutingModule} from './app-routing/app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DateTimeType, ModuleConfiguration} from 'hateoas-navigator';
-import {PropertyConfigBuilder} from 'hateoas-navigator';
-import {HalNavigatorModule} from 'hateoas-navigator';
+import {DateTimeType, FormFieldType, HalNavigatorModule, ModuleConfiguration, PropertyConfigBuilder} from 'hateoas-navigator';
 import {ResourceComponentsModule} from 'resource-components';
+import {QueryConfigBuilder} from 'hateoas-navigator/hal-navigator/config/query-config-builder';
 
 const halNavConfig: ModuleConfiguration = {
   updateMethod: 'PATCH',
@@ -33,6 +32,15 @@ const halNavConfig: ModuleConfiguration = {
         .withProperty('user', {associatedResourceName: 'users', title: 'User'})
         .withProperty('reactionType', {enumOptions: ['APPROVE', 'UNSURE', 'DENY'], title: 'Reaction'})
         .withTitle('Vote')
+        .build())
+      .withQuery('findByForGroupAndCalendarEntryFromAfterAndCalendarEntryToBefore', new QueryConfigBuilder()
+        .withTitle('Find by group and date')
+        .withParam('from', {title: 'From', type: FormFieldType.DATE_PICKER})
+        .withParam('to', {title: 'To', type: FormFieldType.DATE_PICKER})
+        .build())
+      .withQuery('getNext', new QueryConfigBuilder()
+        .withTitle('Get next suggestions')
+        .withParam('numOfWeeks', {title: 'Num. of weeks', type: FormFieldType.INTEGER})
         .build())
       .build()
   }
