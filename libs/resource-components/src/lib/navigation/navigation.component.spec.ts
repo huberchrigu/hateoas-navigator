999f7ccfb948efc87cdb81b71bb383f33ad85d74
huber.chrigu <huber.chrigu@gmail.com>
2020-03-19 07:45:49 +0100

    Added missing mock

--- libs/resource-components/src/lib/navigation/navigation.component.spec.ts
+++ libs/resource-components/src/lib/navigation/navigation.component.spec.ts
@@ -2,7 +2,8 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {NavigationComponent} from './navigation.component';
 import {ResourceService, NavigationFactory} from 'hateoas-navigator';
-import {of} from 'rxjs/index';
+import {of} from 'rxjs';
+import {LoginService} from './login/login.service';
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
@@ -14,12 +15,15 @@ describe('NavigationComponent', () => {
     } as NavigationFactory)
   } as ResourceService;
 
+  const loginMock = jasmine.createSpyObj<LoginService>(['isLoggedIn']);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [NavigationComponent],
       schemas: [NO_ERRORS_SCHEMA],
       providers: [
-        {provide: ResourceService, useValue: serviceMock}
+        {provide: ResourceService, useValue: serviceMock},
+        {provide: LoginService, useValue: loginMock}
       ]
     })
       .compileComponents();
