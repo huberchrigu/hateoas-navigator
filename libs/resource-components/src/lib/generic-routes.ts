0db08f105d7908c9d9f86e3f8698884e342c66cf
huber.chrigu <huber.chrigu@gmail.com>
2020-05-01 15:18:58 +0200

    Make single route configs reusable

--- libs/resource-components/src/lib/generic-routes.ts
+++ libs/resource-components/src/lib/generic-routes.ts
@@ -7,37 +7,48 @@ import {ResourceDescriptorResolverService} from 'hateoas-navigator';
 import {ResourceObjectResolverService} from 'hateoas-navigator';
 import {CollectionResolverService} from 'hateoas-navigator';
 
+/**
+ * Provides route configurations for editing, creating, listing and viewing resource items.
+ */
 export class GenericRoutes {
+  static readonly NEW_ITEM = {
+    path: ':' + RouteParams.RESOURCE_PARAM + '/new',
+    component: ResourceFormComponent,
+    resolve: {
+      resourceDescriptor: ResourceDescriptorResolverService
+    }
+  };
+
+  static readonly ITEM_DETAILS = {
+    path: `:${RouteParams.RESOURCE_PARAM}/:${RouteParams.ID_PARAM}`,
+    component: ResourceItemComponent,
+    resolve: {
+      resourceObject: ResourceObjectResolverService
+    }
+  };
+
+  static readonly EDIT_ITEM = {
+    path: `:${RouteParams.RESOURCE_PARAM}/:${RouteParams.ID_PARAM}/edit`,
+    component: ResourceFormComponent,
+    resolve: {
+      resourceObject: ResourceObjectResolverService
+    }
+  };
+
+  static readonly COLLECTION = {
+    path: ':' + RouteParams.RESOURCE_PARAM,
+    component: ResourceListComponent,
+    resolve: {
+      collectionAdapter: CollectionResolverService
+    }
+  };
+
   public static get(): Routes {
     return [
-      {
-        path: ':' + RouteParams.RESOURCE_PARAM + '/new',
-        component: ResourceFormComponent,
-        resolve: {
-          resourceDescriptor: ResourceDescriptorResolverService
-        }
-      },
-      {
-        path: `:${RouteParams.RESOURCE_PARAM}/:${RouteParams.ID_PARAM}`,
-        component: ResourceItemComponent,
-        resolve: {
-          resourceObject: ResourceObjectResolverService
-        }
-      },
-      {
-        path: `:${RouteParams.RESOURCE_PARAM}/:${RouteParams.ID_PARAM}/edit`,
-        component: ResourceFormComponent,
-        resolve: {
-          resourceObject: ResourceObjectResolverService
-        }
-      },
-      {
-        path: ':' + RouteParams.RESOURCE_PARAM,
-        component: ResourceListComponent,
-        resolve: {
-          collectionAdapter: CollectionResolverService
-        }
-      }
+      GenericRoutes.NEW_ITEM,
+      GenericRoutes.ITEM_DETAILS,
+      GenericRoutes.EDIT_ITEM,
+      GenericRoutes.COLLECTION
     ];
   }
 }
